57a5974e16a1154d3e42921fb264dbf57978c37e
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-05-19 12:21:26 +0200

    Testing Almost Done

--- Testing.sql
+++ Testing.sql
@@ -270,39 +270,204 @@ SELECT [CustomerID]
 
   SET identity_insert [Sales].[Customer] OFF
 
--- USE AdventureWorksDWH
--- GO
+USE AdventureWorksDWH
+GO
 
--- CREATE SCHEMA ETL
--- GO
+CREATE SCHEMA ETL
+GO
 
--- CREATE TABLE ETL.[LogUpdate](
--- 	[Table] [nvarchar](50) NULL,
---     [LastUpdate] INT NULL,
--- ) on [PRIMARY]
--- GO
+CREATE TABLE ETL.[LogUpdate](
+	[Table] [nvarchar](50) NULL,
+    [LastUpdate] INT NULL,
+) on [PRIMARY]
+GO
 
--- INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('DimProduct', 19980506)
--- GO
--- INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('DimCustomer', 19980506)
--- GO
--- INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('DimEmployee', 19980506)
--- GO
--- INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('FactSale', 19980506)
--- GO
+INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('DimProduct', 19980506)
+GO
+INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('DimCustomer', 19980506)
+GO
+INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('DimEmployee', 19980506)
+GO
+INSERT INTO ETL.[LogUpdate] ([Table], [LastUpdate]) VALUES ('FactSale', 19980506)
+GO
+
+ALTER TABLE EDW.DimProduct DROP COLUMN ValidFrom, ValodTo;
+
+ALTER TABLE EDW.[DimProduct] ADD ValidFrom INT, ValidTo INT
+GO
+
+ALTER TABLE EDW.[DimCustomer] ADD ValidFrom INT, ValidTo INT
+GO
+
+ALTER TABLE EDW.[DimEmployee] ADD ValidFrom INT, ValidTo INT
+GO
 
 
--- ALTER TABLE EDW.[DimProduct] ADD ValidFrom INT, ValodTo INT
--- GO
--- ALTER TABLE EDW.[DimCustomer] ADD ValidFrom INT, ValodTo INT
--- GO
--- ALTER TABLE EDW.[DImEmployee] ADD ValidFrom INT, ValodTo INT
--- GO
+UPDATE EDW.[DimProduct] SET ValidFrom = 20100430, ValidTo = 20991231
+GO
+UPDATE EDW.[DimCustomer] SET ValidFrom = 20100430, ValidTo = 20991231
+GO
+UPDATE EDW.[DimEmployee] SET ValidFrom = 20100430, ValidTo = 20991231
+GO
 
--- UPDATE EDW.[DimProduct] SET ValidFrom = 20100430, ValodTo = 20991231
--- GO
--- UPDATE EDW.[DimCustomer] SET ValidFrom = 20100430, ValodTo = 20991231
--- GO
--- UPDATE EDW.[DimEmployee] SET ValidFrom = 20100430, ValodTo = 20991231
--- GO
 
+SELECT MIN(ModifiedDate)
+FROM AdventureWorks2019.Sales.Customer
+GO
+
+DECLARE @LastLoadDate INT
+	SET @LastLoadDate = (SELECT MAX(@LastLoadDate)
+		FROM ETL.LogUpdate
+		WHERE [Table] = 'DimCustomer'
+	)
+
+DECLARE @NewLoadDate INT
+    SET @NewLoadDate = CONVERT(CHAR(8), GETDATE(), 112)
+
+DECLARE @FutureDate INT
+    SET @FutureDate = 20991231
+
+INSERT INTO EDW.DimCustomer
+           (
+                CustomerId,
+                CustomerName,
+                ValidFrom,
+                ValidTo
+           )
+
+SELECT CustomerId,
+       CustomerName,
+       @NewLoadDate,
+       @FutureDate
+
+FROM STAGE.DimCustomer
+    WHERE CustomerId IN (
+        SELECT CustomerId FROM STAGE.DimCustomer EXCEPT 
+        SELECT CustomerId FROM EDW.DimCustomer 
+        WHERE ValidTo = 20991231
+    )
+GO
+
+DECLARE @LastLoadDate INT
+	SET @LastLoadDate = (SELECT MAX(@LastLoadDate)
+		FROM ETL.LogUpdate
+		WHERE [Table] = 'DimEmployee'
+	)
+
+DECLARE @NewLoadDate INT
+    SET @NewLoadDate = CONVERT(CHAR(8), GETDATE(), 112)
+
+DECLARE @FutureDate INT
+    SET @FutureDate = 20991231
+
+INSERT INTO EDW.DimEmployee
+           (
+              EmployeeId,
+			  EmployeeName,
+			  City,
+			  Country,
+			  Title,
+			  ValidFrom,
+			  ValidTo
+           )
+
+SELECT	EmployeeId,
+		EmployeeName,
+		City,
+		Country,
+		Title,
+		@NewLoadDate,
+		@FutureDate
+
+FROM STAGE.DimEmployee
+    WHERE EmployeeId IN (
+        SELECT EmployeeId FROM STAGE.DimEmployee EXCEPT 
+        SELECT EmployeeId FROM EDW.DimEmployee
+        WHERE ValidTo = 20991231
+    )
+GO
+
+
+DECLARE @LastLoadDate INT
+	SET @LastLoadDate = (SELECT MAX(@LastLoadDate)
+		FROM ETL.LogUpdate
+		WHERE [Table] = 'DimProduct'
+	)
+
+DECLARE @NewLoadDate INT
+    SET @NewLoadDate = CONVERT(CHAR(8), GETDATE(), 112)
+
+DECLARE @FutureDate INT
+    SET @FutureDate = 20991231
+
+INSERT INTO EDW.DimProduct
+           (
+              ProductId,
+			  ProductName,
+			  ProductCategory,
+			  Price,
+			  ValidFrom,
+			  ValidTo
+           )
+
+SELECT	ProductId,
+			  ProductName,
+			  ProductCategory,
+			  Price,
+		@NewLoadDate,
+		@FutureDate
+
+FROM STAGE.DimProduct
+    WHERE ProductId IN (
+        SELECT ProductId FROM STAGE.DimProduct EXCEPT 
+        SELECT ProductId FROM EDW.DimProduct
+        WHERE ValidTo = 20991231
+    )
+GO
+
+
+--Incremental Load Fact Table
+DECLARE @LastLoadDate DATETIME
+	SET @LastLoadDate = (SELECT [Date]
+		FROM AdventureWorksDWH.EDW.DimDate
+		WHERE DateId IN(SELECT MAX(@LastLoadDate) 
+		FROM ETL.LogUpdate WHERE [Table] = 'FactSale')
+	)
+GO
+
+DECLARE @LastLoadDate DATETIME
+	SET @LastLoadDate = (SELECT [Date]
+		FROM EDW.DimDate
+		WHERE DateId = 19980506
+	)
+
+DELETE FROM STAGE.FactSale
+
+INSERT INTO STAGE.FactSale
+	(
+		CustomerId,
+		EmployeeId,
+		ProductId,
+		OrderId,
+		Quantity,
+		UnitPrice,
+		LineTotal,
+		OrderDate
+	)
+SELECT 
+	DC.CustomerId,
+	ISNULL (DE.EmployeeId, 1) AS EmployeeId,
+	DP.ProductId,
+	sod.SalesOrderID AS OrderId,
+	sod.OrderQty,
+	sod.UnitPrice,
+	sod.LineTotal,
+	soh.OrderDate
+
+FROM AdventureWorks2019.Sales.SalesOrderHeader soh
+	LEFT JOIN AdventureWorks2019.Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+	LEFT JOIN AdventureWorksDWH.STAGE.DimCustomer DC ON DC.CustomerId = soh.CustomerID
+	LEFT JOIN AdventureWorksDWH.STAGE.DimEmployee DE ON DE.EmployeeId = soh.SalesPersonID
+	INNER JOIN AdventureWorksDWH.STAGE.DimProduct DP ON DP.ProductId = sod.ProductID
+	WHERE soh.OrderDate > (@LastLoadDate)
+GO
